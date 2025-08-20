942f7fed09c8415bc68460e449080785bf068ee7
oguzademoglu <63853844+oguzademoglu@users.noreply.github.com>
2025-08-20 08:58:01 +0300

    feat: player basics and entitystate, state machine create. (#1)

--- Assets/LV01/Scripts/EntityState.cs
+++ Assets/LV01/Scripts/EntityState.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
 
-public class EntityState : MonoBehaviour
+public abstract class EntityState
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected Player player;
+    protected StateMachine stateMachine;
+    protected string stateName;
+    protected int animBoolHash;
+    protected Rigidbody2D rb;
+    protected Animator anim;
+    protected PlayerInputs playerInputs;
+    public EntityState(Player player, StateMachine stateMachine, string stateName)
     {
-        
+        this.player = player;
+        this.stateMachine = stateMachine;
+        this.stateName = stateName;
+        rb = player.Rb;
+        anim = player.Anim;
+        animBoolHash = Animator.StringToHash(stateName);
+        playerInputs = player.PlayerInputs;
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Enter()
     {
-        
+        anim.SetBool(animBoolHash, true);
+    }
+    public virtual void Update() { }
+
+    public virtual void PhysicsUpdate()
+    {
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+    }
+    public virtual void Exit()
+    {
+        anim.SetBool(animBoolHash, false);
     }
 }
