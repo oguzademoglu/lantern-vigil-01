bc8a6e60a43a39a77fdbc5ad5cbc1a8c76eef575
oguzademoglu <oguzademoglu1@gmail.com>
2025-08-22 15:40:32 +0300

    player_basicAttackState updated.

--- Assets/LV01/Content/Characters/The SwordMaster/Scripts/Player/PlayerStates/Player_BasicAttackState.cs
+++ Assets/LV01/Content/Characters/The SwordMaster/Scripts/Player/PlayerStates/Player_BasicAttackState.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class Player_BasicAttackState : PlayerState
@@ -48,52 +47,4 @@ public class Player_BasicAttackState : PlayerState
     }
 }
 
-using System;
-using UnityEngine;
 
-public class Player_BasicAttackState : EntityState
-{
-    public Player_BasicAttackState(Player player, StateMachine stateMachine, string stateName) : base(player, stateMachine, stateName)
-    {
-    }
-
-    private float attackVelocityTimer;
-
-    public override void Enter()
-    {
-        base.Enter();
-        player.swordCollider.SetActive(true);
-        ApplyAttackVelocity();
-    }
-
-
-    public override void PhysicsUpdate()
-    {
-        base.PhysicsUpdate();
-        HandleAttackVelocity();
-
-        if (triggerCalled)
-        {
-            if (player.GroundDetected) stateMachine.ChangeState(player.IdleState);
-        }
-    }
-
-    public override void Exit()
-    {
-        base.Exit();
-        player.swordCollider.SetActive(false);
-    }
-
-    void HandleAttackVelocity()
-    {
-        attackVelocityTimer -= Time.fixedDeltaTime;
-        if (attackVelocityTimer < 0f)
-            player.SetVelocity(0, rb.linearVelocity.y);
-    }
-
-    void ApplyAttackVelocity()
-    {
-        attackVelocityTimer = player.attackVelocityDuration;
-        player.SetVelocity(player.attackVelocity.x * player.facingDirection, player.attackVelocity.y);
-    }
-}
